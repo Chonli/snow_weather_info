7b6fbcfbe07b63cba42a38bd46abdb194a300d0e
Chonli <Chonli@users.noreply.github.com>
2020-01-15 14:17:16 +0100

    parse list of station

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/station.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,6 +28,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+//https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Nivo/nivo.20200114.csv
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -32,7 +37,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(),
+        child: MaterialButton(
+          child: Text('Test'),
+          onPressed: () async {
+            final response = await http.get(
+                'https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Nivo/postesNivo.json');
+
+            if (response.statusCode == 200) {
+              print(response.body);
+              var data = json.decode(response.body);
+              var rest = data["features"] as List;
+              print(rest);
+              var list = rest
+                  .map<Station>((json) => Station.fromJson(json["properties"]))
+                  .toList();
+              print(list);
+            } else {
+              throw Exception('Failed to load post');
+            }
+          },
+        ),
       ),
     );
   }
---
+++ lib/model/station.dart
@@ -0,0 +1,29 @@
+import 'package:latlong/latlong.dart';
+
+class Station {
+  //"Latitude": "46.341167", "Longitude": "6.708167", "ID": "07454", "Altitude": "1535", "Nom": "Bernex"
+  String _id;
+  String _name;
+  LatLng _position;
+  int _altitude;
+
+  Station(this._id, this._name, this._position, this._altitude);
+
+  String get id => _id;
+  String get name => _name;
+  LatLng get position => _position;
+  int get altitude => _altitude;
+
+  Station.fromJson(Map<String, dynamic> json) {
+    _id = json['ID'];
+    _name = json['Nom'];
+    _position =
+        LatLng(double.parse(json['Latitude']), double.parse(json['Longitude']));
+    _altitude = int.parse(json['Altitude']);
+  }
+
+  @override
+  String toString() {
+    return '$_id - $_name : $_altitude m, $_position';
+  }
+}
