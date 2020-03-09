3a44e2a3c63577c19202cab57cf077884c2f6e89
Chonli <Chonli@users.noreply.github.com>
2020-03-09 23:13:27 +0100

    Improve pdf render

--- lib/ui/avalanche_bulletin_page.dart
+++ lib/ui/avalanche_bulletin_page.dart
@@ -1,7 +1,5 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:pdf_render/pdf_render_widgets.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
 import 'package:snow_weather_info/model/avalanche_bulletin.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,13 +7,13 @@ class AvalancheBulletinPage extends StatelessWidget {
   final AvalancheBulletin bulletin;
   const AvalancheBulletinPage(this.bulletin);
 
-  Future<Uint8List> _loadBulletin() async {
+  Future<PDFDocument> _loadBulletin() async {
     //download file
     try {
       final response = await http.get(bulletin.url);
 
       // Write the file
-      return response.bodyBytes;
+      return PDFDocument.openData(response.bodyBytes);
     } catch (e) {
       return null;
     }
@@ -28,30 +26,25 @@ class AvalancheBulletinPage extends StatelessWidget {
         title: Text(bulletin.massifName),
       ),
       body: Center(
-        child: FutureBuilder<Uint8List>(
-            future: _loadBulletin(),
-            builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return CircularProgressIndicator();
-                default:
-                  if (snapshot.hasError || !snapshot.hasData) {
+        child: FutureBuilder<PDFDocument>(
+          future: _loadBulletin(),
+          builder: (BuildContext context, AsyncSnapshot<PDFDocument> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return CircularProgressIndicator();
+              default:
+                if (snapshot.hasError || !snapshot.hasData) {
+                  return Text('Erreur de chargement');
+                } else {
+                  try {
+                    return PDFView(document: snapshot.data);
+                  } catch (e) {
                     return Text('Erreur de chargement');
-                  } else {
-                    try {
-                      return PdfDocumentLoader(
-                        data: snapshot.data,
-                        pageNumber: 1,
-                        calculateSize: (pageWidth, pageHeight, aspectRatio) =>
-                            Size(pageWidth * aspectRatio,
-                                pageHeight * aspectRatio),
-                      );
-                    } catch (e) {
-                      return Text('Erreur de chargement');
-                    }
                   }
-              }
-            }),
+                }
+            }
+          },
+        ),
       ),
     );
   }
